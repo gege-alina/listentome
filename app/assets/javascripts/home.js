80a32db5b600328bb929e9ce76a17b0c8e44c8a7
Razvan Dumitru <razvandumitru@Workspace-Ubuntu.(none)>
2013-12-14 15:36:27 +0200

    Init Javascript & Fix turbolinks fail

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,29 @@
+var HomeIndex = function () {
+        this._initHome();
+};
+
+HomeIndex.prototype._initHome = function(){
+    this._addDelegates();
+};
+
+HomeIndex.prototype.selectors = {
+
+	listSelector : '.list-group',
+	listItemSelector : '.list-group-item'
+};
+
+HomeIndex.prototype._addDelegates = function(){
+
+$(this.selectors.listSelector).find(this.selectors.listItemSelector).on('click', $.proxy(this._preventDefaultListItem,this));
+
+};
+
+HomeIndex.prototype._preventDefaultListItem = function(e){
+// nu mai crapa, turbolinks 	
+e.preventDefault();
+
+};
+
+$(document).ready(function () {
+        var ctrl = new HomeIndex();
+ });
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
 	def index
+		puts "ceva"
 	end
 end
