88330f787ca2e52852f1a408e3fc654a652666c3
smariussorin <smarius.sorin@yahoo.com>
2014-02-06 23:27:53 +0200

    added most settings for facebook connect #26

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -228,7 +228,10 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-
+ 
+  require "omniauth-facebook"
+  config.omniauth :facebook, "204947999705002", "9f99cba6d42551edc069b34dfa19887f"
+  
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
--- db/migrate/20131214123006_add_columns_to_user.rb
+++ db/migrate/20131214123006_add_columns_to_user.rb
@@ -5,6 +5,7 @@ class AddColumnsToUser < ActiveRecord::Migration
 	add_column :users, :points, :integer
   	add_column :users, :avatar_url, :string
   	add_column :users, :username, :string
-
+  	add_column :users, :provider, :string
+  	add_column :users, :uid, :string
   end
 end
