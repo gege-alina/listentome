8f1bc640e89aa9ed771432bd10439a8f8482b303
= <radumvlad@yahoo.com>
2014-02-08 20:26:57 +0200

    #function userPoints

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,4 +12,16 @@ class HomeController < ApplicationController
 	  end
 	  
 	end
+
+	def addPointsToUser
+	  @user_song = UserSong.where(song_id: song_id_param)
+
+	  @user = User.where(id: @user_song.user_id)
+	  @user.points += @user_song.boost
+	  @user.save
+
+	  @user_song.boost = 0
+	  @user_song.save
+	end
+
 end
