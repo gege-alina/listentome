2974363193b3656f6dab751a32813fada25d0004
= <radumvlad@yahoo.com>
2014-02-08 23:13:53 +0200

    #first ajax call

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Listentome::Application.routes.draw do
   resources :songs
   get "static_pages/about"
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+
+  match 'songs/boostSong' => 'songs#boostSong', :via => [:post], :as => 'songs/boostSong'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
