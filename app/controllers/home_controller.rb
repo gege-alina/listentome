a2b9863fc310e8aab1a513ceab9e6c974e7e3585
= <radumvlad@yahoo.com>
2014-02-08 23:32:15 +0200

    #addPointsToUser

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -21,14 +21,14 @@ class HomeController < ApplicationController
 	end
 
 	def addPointsToUser
-	  @user_song = UserSong.where(song_id: song_id_param)
+	  user_song = UserSong.where(song_id: params[:song_id]).first
 
-	  @user = User.where(id: @user_song.user_id)
-	  @user.points += @user_song.boost
-	  @user.save
+	  user = User.find(user_song.user_id)
+	  user.points += user_song.boost
+	  user.save
 
-	  @user_song.boost = 0
-	  @user_song.save
+	  user_song.boost = 0
+	  user_song.save
 	end
 
 end
